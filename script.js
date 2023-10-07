51fa61a847d417ce9acd8ece1cdef94a1dfe17f6
MilindaAppuhamy <milindaappuhamy.vodafone@gmail.com>
2023-10-07 15:05:55 +0100

    Done.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Write your code here
+const sub = document.querySelector(".subtitle");
+const bulbs = document.querySelectorAll(".lightbulb");
+
+let count = 0;
+
+bulbs.forEach((bulb) => {
+  bulb.addEventListener("mouseenter", () => {
+    bulb.classList.toggle("active");
+    count++;
+
+    count === 1
+      ? (sub.innerHTML = `You've clicked the lightbulbs ${count} time`)
+      : (sub.innerHTML = `You've clicked the lightbulbs ${count} times`);
+  });
+});
